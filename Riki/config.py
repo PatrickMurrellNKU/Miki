c156c4eeecc5627345f9fc01d657275112b203d3
Griffin Quarles <griffinq178@gmail.com>
2021-11-18 14:12:36 -0500

    Fix project setup

--- Riki/config.py
+++ Riki/config.py
@@ -1,10 +1,12 @@
+import os
+
 # encoding: utf-8
 
 SECRET_KEY='a unique and long key'
 TITLE='Miki'
 HISTORY_SHOW_MAX=30
 PIC_BASE = '/static/content/'
-CONTENT_DIR = 'C:\\Users\\patrick.murrell\\PycharmProjects\\Miki\\Riki\\content'
-USER_DIR = 'C:\\Users\\patrick.murrell\\PycharmProjects\\Miki\\Riki\\user'
+CONTENT_DIR = os.getcwd() + '/content'
+USER_DIR = os.getcwd() + '/user'
 NUMBER_OF_HISTORY = 5
 PRIVATE = True
