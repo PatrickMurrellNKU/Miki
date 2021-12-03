daee1c652bc2e9baa60fdbb78c0996d8d41707da
Schmooly9 <70486146+Schmooly9@users.noreply.github.com>
2021-12-03 12:34:40 -0500

    Added comments to featured.py

--- Riki/wiki/web/featured.py
+++ Riki/wiki/web/featured.py
@@ -1,17 +1,20 @@
 from wiki.web import current_wiki
 import time
 
-
+# Main method to handle displaying the featured page
 def feature():
+    # First checks to remove home page if there are other pages
     pages = remove_home()
     if not pages:
+        # If pages was empty, then home is the only page thus it should be displayed in place of the featured page
         index = current_wiki.index()
         page = index[0]
     else:
+        # If pages wasn't empty, display the proper page
         page = choose(pages)
     return page
 
-
+# Obtains a list of all the pages in the wiki and removes the home page since it doesn't need to be a featured page
 def remove_home():
     index = current_wiki.index()
     pages = []
@@ -21,14 +24,14 @@ def remove_home():
     del index[i]
     return index
 
-
+# Using time, chooses the correct page to display.  The page will change every new day
 def choose(pages):
     day = int(get_time())
     length = len(pages)
     value = day % length
     return pages[value]
 
-
+# Gets the time in a usable format for choose() method
 def get_time():
     t = time.localtime()
     day = time.strftime("%d", t)
